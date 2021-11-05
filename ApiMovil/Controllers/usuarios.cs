cf34ae67fba02113660a60450648b5a4a15ede3b
gustavo <gustavo.vtamayo@outlook.com>
2021-11-05 17:52:47 -0600

    first commit

---
+++ ApiMovil/Controllers/usuarios.cs
@@ -0,0 +1,49 @@
+﻿using ApiMovil.Models;
+using ApiMovil.Repositorio;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiMovil.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class usuarios : ControllerBase
+    {
+        // GET: api/<usuarios>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            RPusuario rpUsuario = new RPusuario();
+            return Ok(rpUsuario.ObtenerClientes());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            RPusuario rpUsuario = new RPusuario();
+
+            var cliRet = rpUsuario.ObtenerUsuario(id);
+
+            if (cliRet == null)
+            {
+                var nf = NotFound("El cliente " + id.ToString() + " no existe.");
+                return nf;
+            }
+
+            return Ok(cliRet);
+        }
+
+        [HttpPost("agregar")]
+        public IActionResult AgregarCliente(Usuario nuevoUsuario)
+        {
+            RPusuario rpCli = new RPusuario();
+            rpCli.Agregar(nuevoUsuario);
+            return CreatedAtAction(nameof(AgregarCliente), nuevoUsuario);
+        }
+    }
+}
---
+++ ApiMovil/Repositorio/RPusuario.cs
@@ -0,0 +1,35 @@
+﻿using ApiMovil.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiMovil.Repositorio
+{
+    public class RPusuario
+    {
+        public static List<Usuario> _listaUsuarios = new List<Usuario>()
+        {
+            new Usuario() { Id = 1, Nombre = "Cliente 1" , Apellido = "Apellido 1" },
+            new Usuario() { Id = 2, Nombre = "Cliente 2" , Apellido = "Apellido 2" },
+            new Usuario() { Id = 3, Nombre = "Cliente 3" , Apellido = "Apellido 3" }
+        };
+
+        public IEnumerable<Usuario> ObtenerClientes()
+        {
+            return _listaUsuarios;
+        }
+
+        public Usuario ObtenerUsuario(int id)
+        {
+            var cliente = _listaUsuarios.Where(cli => cli.Id == id);
+
+            return cliente.FirstOrDefault();
+        }
+
+        public void Agregar(Usuario nuevoUsuario)
+        {
+            _listaUsuarios.Add(nuevoUsuario);
+        }
+    }
+}
